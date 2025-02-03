e3f6eedc1f5a1bcf560e09616548b43477893427
JeromeMaligaya <jeromehapi_97@yahoo.com>
2025-02-03 12:16:48 +0100

    fixed migration for accounts table

--- database/migrations/2025_02_03_110936_create_accounts_table.php
+++ database/migrations/2025_02_03_110936_create_accounts_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_account', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->after("id")->constrained();
+            $table->foreignId("user_id")->constrained()->onDelete('cascade');
             $table->decimal('current_balance')->default(0);
             $table->timestamps();
         });
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_account');
+        Schema::dropIfExists('accounts');
     }
 };
