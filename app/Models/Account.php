34d44a77c6f97f5176ccfd42c4485f22aa1aab25
JeromeMaligaya <jeromehapi_97@yahoo.com>
2025-02-03 17:10:58 +0100

    added model Account with fillable and relations

---
+++ app/Models/Account.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["user_id", "current_balance"];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
