a8b046726425afe266ff14161a6a12e4994b0385
JeromeMaligaya <jeromehapi_97@yahoo.com>
2025-02-03 17:18:04 +0100

    added model Transaction with fillable and relations

---
+++ app/Models/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ["account_id", "type", "amount", "description", "date"];
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Catergory::class);
+    }
+}
