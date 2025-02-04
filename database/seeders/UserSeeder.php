1e56842c7c0c52d5e23e70998ee17a6e09c3def7
JeromeMaligaya <jeromehapi_97@yahoo.com>
2025-02-04 15:29:28 +0100

    added UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Jerome Maligaya',
+            'email' => 'jerome_hapi@yahoo.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
